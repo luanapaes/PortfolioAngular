76057846ab20af4b5c861c82328f5fc6dd177e59
Luana Paes <luanafranca598@gmail.com>
2024-04-28 19:56:52 -0300

    Adicionando projetos à aba de experiências

--- angular-portfolio/src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ angular-portfolio/src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IExperiences } from '../../interface/IExperiences.interface';
 
 @Component({
   selector: 'app-experiences',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
-
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      sumary: {
+        strong: "Projeto CRUD",
+        p: "Angular + Spring Boot"
+      },
+      text: '<p> Desenvolvi um sistema de gerenciamento de de usuários utilizando Angular no frontend e Spring Boot no backend.</p>'
+    },
+    {
+      sumary: {
+        strong: "FitInsight",
+        p: "HTML, CSS, Javascript, Python e Django"
+      },
+      text: '<p>Junto aos meus colegas da faculdade desenvolvi um sistema para atender às necessidades de redes de academia. Utilizando uma IA para prever a frequência dos alunos, proporcionando maior controle sobre os alunos pela academia, utilizando no frontend HTML, CSS, e Javascript, Python no desenvolvimento da IA e Django no backend.</p>'
+    },
+    {
+      sumary: {
+        strong: "IDE de Fluxograma",
+        p: "HTML, CSS e Javascript"
+      },
+      text: 'Desenvolvi o frontend para uma IDE de fluxograma como parte de um projeto de extensão na faculdade.A IDE foi projetada para auxiliar estudantes iniciantes em programação.</p>'
+    }
+  ])
 }
