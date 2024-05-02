e4d20ab32296fc15759ae3bb65160ac0933a6efb
Luana Paes <luanafranca598@gmail.com>
2024-05-02 20:13:14 -0300

    Lógica do componente

--- angular-portfolio/src/app/modules/portfolio/dialog/dialog-project/dialog-project.component.ts
+++ angular-portfolio/src/app/modules/portfolio/dialog/dialog-project/dialog-project.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects.interface';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-project',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './dialog-project.component.html',
   styleUrl: './dialog-project.component.scss'
 })
-export class DialogProjectComponent {
+export class DialogProjectComponent implements OnInit{
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private _data: IProjects,
+    private _dialogRef: MatDialogRef<DialogProjectComponent>
+  ){}
 
+  public getData = signal<IProjects | null >(null);
+
+  ngOnInit(): void {
+      this.getData.set(this._data);
+  }
+
+  public closeDialog(){
+    this._dialogRef.close()
+  }
 }
